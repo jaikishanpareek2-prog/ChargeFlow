e302acdae0890401bbf47e46ffb11fa2a314b6c7
jaikishanpareek2-prog <jaikishanpareek2@gmail.com>
2026-09-24 23:51:48 +0300

    Fix ChargeFlow media fallback Bolt import

--- .github/patches/MediaRenderer.kt
+++ .github/patches/MediaRenderer.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Icon
+import androidx.compose.material.icons.filled.Bolt
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
